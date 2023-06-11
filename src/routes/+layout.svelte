0e8969b30c68cbef146f6361e58cbc0e672cb8fa
klarak04 <107683806+klarak04@users.noreply.github.com>
2023-06-11 19:58:48 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '../theme.postcss';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/all.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import {
+		AppBar,
+		AppShell,
+		Avatar,
+		Drawer,
+		Modal,
+		Toast,
+		drawerStore
+	} from '@skeletonlabs/skeleton';
+	import Navigation from '$lib/components/Navigation.svelte';
+
+	function drawerOpen(): void {
+		drawerStore.open();
+	}
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	
+</script>
+
+<Toast position="tr" />
+<Modal />
+<Drawer style="width: 10vh;">
+	<Navigation />
+</Drawer>
+
+<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<button class="md:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+					<span>
+						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+							<rect width="100" height="20" />
+							<rect y="30" width="100" height="20" />
+							<rect y="60" width="100" height="20" />
+						</svg>
+					</span>
+				</button>
+				<strong class="text-xl uppercase">LinkUp</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<LightSwitch />
+				<a href="my-profile">
+					<Avatar initials="KK" width="w-10" background="bg-primary-500" />
+				</a>
+				
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<Navigation />
+	</svelte:fragment>
+	<!-- Router Slot -->
+	<div class="container p-10 mx-auto">
+		<slot />
+	</div>
+	<!-- ---- / ---- -->
+</AppShell>
---
+++ src/routes/my-profile/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { TabGroup, Tab } from '@skeletonlabs/skeleton';
+    let tabSet: number = 0;
+	import {Avatar,} from '@skeletonlabs/skeleton';
+	let user = 'KK';
+</script>
+
+<div class="user">
+	<Avatar initials={user} width="w-12" background="bg-primary-500" />
+	<div class="user-info">
+		<span>Klara Kulinna</span>
+		<span>@KK</span>
+	</div>
+</div>
+
+<div class="Tabs">
+    
+<TabGroup justify="justify-center" padding="px-20 py-3">
+	<Tab bind:group={tabSet} name="tab1" value={0}>Posts</Tab>
+	<Tab bind:group={tabSet} name="tab2" value={1}>Follower</Tab>
+	<Tab bind:group={tabSet} name="tab3" value={2}>Likes</Tab>
+	<!-- Tab Panels --->
+	<svelte:fragment slot="panel">
+		{#if tabSet === 0}
+			(tab panel 1 contents)
+		{:else if tabSet === 1}
+			(tab panel 2 contents)
+		{:else if tabSet === 2}
+			(tab panel 3 contents)
+		{/if}
+	</svelte:fragment>
+</TabGroup>
+			
+</div>
+
+<style>
+	.user {
+        display: flex;
+        align-items: center;
+    }
+
+    .user-info {
+        margin-left: 8px;
+    }
+</style>
