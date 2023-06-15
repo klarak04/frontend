bfe7b18fbe8ee144f4c717cf5454f390e4edaf1d
marcbudd <marcbuddemeier@gmail.com>
2023-06-15 18:50:03 +0200

    Adding backend

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+    export let data;
+</script>
+
+<h2>My cool user list</h2>
+<ul>
+    {#each data.users as user}
+        <li>
+            {user.id} - {`${user.username}`}
+        </li>
+    {/each}
+</ul>
+
+
+<!-- <form method="post">
+    <input type="hidden" name="id" value={task.id} />
+    <input
+        type="checkbox"
+        name="completed"
+        checked={task.completed}
+    />
+    {task.id} - {`${task.name}: ${task.description}`}
+    <button formaction="?/update">Update</button>
+</form> -->
