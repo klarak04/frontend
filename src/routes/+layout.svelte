09ddd0c070177bcd113760feecb30d009c4fad8c
klarak04 <107683806+klarak04@users.noreply.github.com>
2023-06-19 09:04:15 +0200

    neue Seiten

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,45 +20,46 @@
 		drawerStore.open();
 	}
 	import { LightSwitch } from '@skeletonlabs/skeleton';
-	
+
 </script>
 
-<Toast position="tr" />
-<Modal />
-<Drawer style="width: 10vh;">
-	<Navigation />
-</Drawer>
 
-<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
-	<svelte:fragment slot="header">
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<button class="md:hidden btn btn-sm mr-4" on:click={drawerOpen}>
-					<span>
-						<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-							<rect width="100" height="20" />
-							<rect y="30" width="100" height="20" />
-							<rect y="60" width="100" height="20" />
-						</svg>
-					</span>
-				</button>
-				<strong class="text-xl uppercase">LinkUp</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<LightSwitch />
-				<a href="my-profile">
-					<Avatar initials="KK" width="w-10" background="bg-primary-500" />
-				</a>
-				
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">
+
+	<Toast position="tr" />
+	<Modal />
+	<Drawer style="width: 10vh;">
 		<Navigation />
-	</svelte:fragment>
-	<!-- Router Slot -->
-	<div class="container p-10 mx-auto">
-		<slot />
-	</div>
-	<!-- ---- / ---- -->
-</AppShell>
+	</Drawer>
+
+	<AppShell slotSidebarLeft="w-0 md:w-52 bg-surface-500/10">
+		<svelte:fragment slot="header">
+			<AppBar>
+				<svelte:fragment slot="lead">
+					<button class="md:hidden btn btn-sm mr-4" on:click={drawerOpen}>
+						<span>
+							<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+								<rect width="100" height="20" />
+								<rect y="30" width="100" height="20" />
+								<rect y="60" width="100" height="20" />
+							</svg>
+						</span>
+					</button>
+					<strong class="text-xl uppercase">LinkUp</strong>
+				</svelte:fragment>
+				<svelte:fragment slot="trail">
+					<LightSwitch />
+					<a href="my-profile">
+						<Avatar initials="KK" width="w-10" background="bg-primary-500" />
+					</a>
+				</svelte:fragment>
+			</AppBar>
+		</svelte:fragment>
+		<svelte:fragment slot="sidebarLeft">
+			<Navigation />
+		</svelte:fragment>
+		<!-- Router Slot -->
+		<div class="container p-10 mx-auto">
+			<slot />
+		</div>
+		<!-- ---- / ---- -->
+	</AppShell>
--- src/routes/my-profile/+page.svelte
+++ src/routes/my-profile/+page.svelte
@@ -1,32 +1,166 @@
 <script lang="ts">
     import { TabGroup, Tab } from '@skeletonlabs/skeleton';
-    let tabSet: number = 0;
 	import {Avatar,} from '@skeletonlabs/skeleton';
-	let user = 'KK';
+	import { onMount } from 'svelte';
+	import 'material-icons/iconfont/material-icons.css';
+	import { writable } from 'svelte/store';
+	import Post from '$lib/components/Post.svelte';
+
+	export let tabSet: number = 0;
+	export let user = 'Klara';
+	export let postcount = '1';
+	export let followercount = '5';
+	export let followedcount = '10';
+	export let bio = '~good vibes~';
+	let id = 1;
+	export let isFavorite = false;
+	export let likes = writable(0);
+
+	export const handleTabChange = (event: CustomEvent<number>) => {
+    tabSet = event.detail;
+  };
+	
+    export const toggleFavorite = () => {
+    isFavorite = !isFavorite;
+	if (isFavorite) {
+		likes.update((value) => value +1)
+	} else {
+		likes.update((value) => value - 1);
+	}
+    };	
+
+	onMount(() => {
+    
+  });
+
+  export const removeItem = (index: number) => {
+		// Entferne den Listeneintrag mit dem angegebenen Index
+		// Hier kannst du die entsprechende Logik zum Entfernen des Listeneintrags implementieren
+	};
 </script>
 
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
 <div class="user">
-	<Avatar initials={user} width="w-12" background="bg-primary-500" />
+	<Avatar initials={user} background="bg-primary-500" />
 	<div class="user-info">
 		<span>Klara Kulinna</span>
-		<span>@KK</span>
+		
 	</div>
+	
 </div>
-
+	<div style="margin-top: 3vh; margin-bottom: 3vh;">{bio}</div>
+	<div class="counts">
+		<span><span class="count">{postcount}</span>Posts</span>
+  		<span><span class="count">{followercount}</span>Followers</span>
+  		<span><span class="count">{followedcount}</span>Followed</span>
+	</div>
+	<button type="button" class="btn btn-sm variant-ghost-primary self-end">Profil bearbeiten</button>
+	<br><br>
 <div class="Tabs">
+
+
     
-<TabGroup justify="justify-center" padding="px-20 py-3">
+<TabGroup justify="justify-center" padding="px-20 py-3" on:change={handleTabChange}>
 	<Tab bind:group={tabSet} name="tab1" value={0}>Posts</Tab>
-	<Tab bind:group={tabSet} name="tab2" value={1}>Follower</Tab>
-	<Tab bind:group={tabSet} name="tab3" value={2}>Likes</Tab>
+	<Tab bind:group={tabSet} name="tab2" value={1}>Followers</Tab>
+	<Tab bind:group={tabSet} name="tab3" value={2}>Following</Tab>
+	<Tab bind:group={tabSet} name="tab4" value={3}>Likes</Tab>
 	<!-- Tab Panels --->
 	<svelte:fragment slot="panel">
 		{#if tabSet === 0}
-			(tab panel 1 contents)
+		<Post />			
 		{:else if tabSet === 1}
-			(tab panel 2 contents)
+			<div class="centered-content">
+				<div class="card p-4" style="width: 50vh;">
+					<nav class="list-nav">
+						<ul>
+							<li>
+								<a href="/?">
+									<Avatar initials="LI" background="bg-primary-500" width="w-10" />
+									<span class="flex-auto">Lina Groth</span>
+									<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined" on:click={() => removeItem(0)}>clear</span></button>
+								</a>
+							</li>
+							<li>
+								<a href="/?">
+									<Avatar initials="MA" background="bg-primary-500" width="w-10" />
+									<span class="flex-auto">Marc Buddemeier</span>
+									<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined" on:click={() => removeItem(0)}>clear</span></button>
+								</a>
+							</li>
+							<li>
+								<a href="/?">
+									<Avatar initials="JE" background="bg-primary-500" width="w-10" />
+									<span class="flex-auto">Jenny</span>
+									<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined" on:click={() => removeItem(0)}>clear</span></button>
+								</a>
+							</li>
+							<li>
+								<a href="/?">
+									<Avatar initials="MA" background="bg-primary-500" width="w-10" />
+									<span class="flex-auto">Marc Buddemeier</span>
+									<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+								</a>
+							</li>
+							<li>
+								<a href="/?">
+									<Avatar initials="MA" background="bg-primary-500" width="w-10" />
+									<span class="flex-auto">Marc Buddemeier</span>
+									<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+								</a>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+
 		{:else if tabSet === 2}
-			(tab panel 3 contents)
+		<div class="centered-content">
+			<div class="card p-4" style="width: 50vh;">
+				<nav class="list-nav">
+					<ul>
+						<li>
+							<a href="/?">
+								<Avatar initials="LI" background="bg-primary-500" width="w-10" />
+								<span class="flex-auto">Lina Groth</span>
+								<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+							</a>
+						</li>
+						<li>
+							<a href="/?">
+								<Avatar initials="MA" background="bg-primary-500" width="w-10" />
+								<span class="flex-auto">Marc Buddemeier</span>
+								<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+							</a>
+						</li>
+						<li>
+							<a href="/?">
+								<Avatar initials="JE" background="bg-primary-500" width="w-10" />
+								<span class="flex-auto">Jenny</span>
+								<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+							</a>
+						</li>
+						<li>
+							<a href="/?">
+								<Avatar initials="MA" background="bg-primary-500" width="w-10" />
+								<span class="flex-auto">Marc Buddemeier</span>
+								<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+							</a>
+						</li>
+						<li>
+							<a href="/?">
+								<Avatar initials="MA" background="bg-primary-500" width="w-10" />
+								<span class="flex-auto">Marc Buddemeier</span>
+								<button type="button" class="btn-icon variant-ghost-warning"><span class="material-icons-outlined">clear</span></button>
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		{:else if tabSet === 3}
+		<Post />	
 		{/if}
 	</svelte:fragment>
 </TabGroup>
@@ -41,5 +175,27 @@
 
     .user-info {
         margin-left: 8px;
+    }
+
+
+	.centered-content {
+   		display: flex;
+    	justify-content: center;
+  		align-items: center;
+    }
+
+	.counts {
+		display: flex;
+		text-align: left;
+		margin-bottom: 3vh;
+	}
+
+	.counts span:not(:last-child) {
+   		margin-right: 5vh;
+    }
+
+	.counts .count {
+    	font-weight: bold;
+    	margin-right: 10px;
     }
 </style>
